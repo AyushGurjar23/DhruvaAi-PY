aa0d18bffa42c4315e94513ef8307fcee585b5ea
Ayush_Gurjar... <153966341+AyushGurjar23@users.noreply.github.com>
2024-08-12 15:20:40 +0530

    Add files via upload

---
+++ Dhruva_main.py
@@ -0,0 +1,185 @@
+import pyttsx3
+import speech_recognition 
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import os
+import pyautogui
+import webbrowser
+import random
+import speedtest
+
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query  = r.recognize_google(audio,language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+def alarm(query):
+    timehere = open("Alarmtext.txt","a")
+    timehere.write(query)
+    timehere.close()
+    os.startfile("alarm.py")
+
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "dhruva" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query = takeCommand().lower()
+                if "go to sleep" in query:
+
+                    speak("Ok sir , You can me call anytime")
+                    break 
+                     
+                elif "open" in query:  
+                    query = query.replace("open","")
+                    query = query.replace("dhruva","")
+                    pyautogui.press("super")
+                    pyautogui.typewrite(query)
+                    pyautogui.sleep(2)
+                    pyautogui.press("enter")
+
+                elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")
+
+                elif "tired" in query:
+                    speak("Playing your favourite songs, sir")
+                    a = (1,2,3) 
+                    b = random.choice(a)
+                    if b==1:
+                        webbrowser.open("https://www.youtube.com/watch?v=f30r1XLTAt0&list=RDf30r1XLTAt0")
+
+                elif "pause" in query:
+                    pyautogui.press("k")
+                    speak("video paused")
+                elif "play" in query:
+                    pyautogui.press("k")
+                    speak("video played")
+                elif "mute" in query:
+                    pyautogui.press("m")
+                    speak("video muted")
+
+                elif "volume up" in query:
+                    from keyboard import volumeup
+                    speak("Turning volume up,sir")
+                    volumeup()
+                elif "volume down" in query:
+                    from keyboard import volumedown
+                    speak("Turning volume down, sir")
+                    volumedown()    
+
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query)       
+           
+
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from SearchNow import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query) 
+
+                elif "news" in query:
+                    from NewsRead import latestnews
+                    latestnews() 
+                elif "whatsapp" in query:
+                    from Whatsapp import sendMessage
+                    sendMessage()       
+
+                elif "temperature" in query:
+                    search = "temperature in delhi"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+                elif "weather" in query:
+                    search = "temperature in delhi"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+
+                elif "set an alarm" in query:
+                    print("input time example:- 10 and 10 and 10")
+                    speak("Set the time")
+                    a = input("Please tell the time :- ")
+                    alarm(a)
+                    speak("Done,sir")    
+
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+                    speak(f"Sir, the time is {strTime}")
+                
+                            
+                elif "finally sleep" in query:
+                    speak("Going to sleep,sir")
+                    exit() 
+
+                elif "remember that" in query:
+                    rememberMessage = query.replace("remember that","")
+                    rememberMessage = query.replace("dhruva","")
+                    speak("You told me to remember that"+rememberMessage)
+                    remember = open("Remember.txt","a")
+                    remember.write(rememberMessage)
+                    remember.close()
+                elif "what do you remember" in query:
+                    remember = open("Remember.txt","r")
+                    speak("You told me to remember that" + rememberMessage.read())    
+               
+                elif "internet speed" in query:
+                    wifi  = speedtest.speedtest()
+                    upload_net = wifi.upload()/1048576         #Megabyte = 1024*1024 Bytes
+                    download_net = wifi.download()/1048576
+                    print("Wifi Upload Speed is", upload_net)
+                    print("Wifi download speed is ",download_net)
+                    speak(f"Wifi download speed is {download_net}")
+                    speak(f"Wifi Upload speed is {upload_net}")
+
+                elif "screenshot" in query:
+                     import pyautogui #pip install pyautogui
+                     im = pyautogui.screenshot()
+                     im.save("ss.jpg")
+
+                
+                
